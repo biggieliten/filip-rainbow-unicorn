b499fb7418dc094e917902dd697d45d3542f0cef
biggieliten <filipisaksson00@gmail.com>
2024-01-16 13:40:50 +0100

    second push

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var animals = ["apa", "orangutang", "gorilla", "get", "giraff"];
+var object = {
+    string: "string",
+    number: 1,
+    bool: true
+};
+console.log(object);
 console.log(animals);
--- app.ts
+++ app.ts
@@ -1,3 +1,17 @@
 let animals: string[] = ["apa" , "orangutang" , "gorilla" , "get", "giraff"];
 
+
+type objectInterface = {  
+	string: string;
+	number: number;
+	bool: boolean;
+};
+
+const object: objectInterface = {
+string: "string",
+number: 1,
+bool: true
+}
+
+console.log(object);
 console.log(animals);
