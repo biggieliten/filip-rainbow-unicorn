fb29d65980e1842dd69623830065a8f11735833c
biggieliten <filipisaksson00@gmail.com>
2024-01-16 14:04:04 +0100

    third push

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var animals = ["apa", "orangutang", "gorilla", "get", "giraff"];
+for (var i = 0; i < animals.length; i++) {
+    console.log(stringFunction(animals[i]));
+}
 var object = {
     string: "string",
     number: 1,
     bool: true
 };
+function stringFunction(string) {
+    return "Hej ".concat(string);
+}
+// console.log(stringFunction());
 console.log(object);
 console.log(animals);
--- app.ts
+++ app.ts
@@ -1,5 +1,11 @@
 let animals: string[] = ["apa" , "orangutang" , "gorilla" , "get", "giraff"];
 
+for(let i = 0; i < animals.length; i++) {
+
+console.log(stringFunction(animals[i]));
+
+}
+
 
 type objectInterface = {  
 	string: string;
@@ -12,6 +18,14 @@ string: "string",
 number: 1,
 bool: true
 }
+
+function stringFunction(string: string): string{
+
+	return `Hej ${string}`
+
+}
+
+// console.log(stringFunction());
 
 console.log(object);
 console.log(animals);
