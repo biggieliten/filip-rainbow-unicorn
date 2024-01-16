bf13704efe77e1a52d20301b241e5b9ed0d94fa2
biggieliten <filipisaksson00@gmail.com>
2024-01-16 14:42:22 +0100

    fixed tsconfig

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,14 @@
-var animals = ["apa", "orangutang", "gorilla", "get", "giraff"];
-for (var i = 0; i < animals.length; i++) {
+let animals = ["apa", "orangutang", "gorilla", "get", "giraff"];
+for (let i = 0; i < animals.length; i++) {
     console.log(stringFunction(animals[i]));
 }
-var object = {
+const object = {
     string: "string",
     number: 1,
     bool: true
 };
 function stringFunction(string) {
-    return "Hej ".concat(string);
+    return `Hej ${string}`;
 }
 // console.log(stringFunction());
 console.log(object);
